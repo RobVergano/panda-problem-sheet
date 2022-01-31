e60bf3850f794c011af94e6749d1b7d6a751c1eb
RobVergano <10097802+RobVergano@users.noreply.github.com>
2022-01-31 20:54:54 +0000

    week 2

---
+++ week2/bmi.py
@@ -0,0 +1,19 @@
+#bmi.py
+#This program calculates the BMI
+#Author: Roberto Vergano
+
+#BMI=Kg/(m)^**2
+# Kg = Variable weight
+# m = Variable height
+
+print ("Welcome to BMI Calculator!")
+#Line 9 to make it more user friendly.
+weight = int(input("Enter your weight (Kg): "))
+height = int(input("Enter your height (cm): "))
+#line 11 and 12 asks the user for weight and height. int assigns a numeric value to the variable.
+heightm = height/100
+#line 14 to change from cm to m
+BMI = weight/(heightm)**2
+#line 16 calculates the BMI with the values of line 11 and 14
+print (BMI)
+#line 18 prints the value of the BMI
